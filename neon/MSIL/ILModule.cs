b0121b526a9796ed643b439201ce2bffa8be71c2
Igor Machado <igor.machado@gmail.com>
2018-10-14 00:52:54 -0300

    more and more logger

--- neon/MSIL/ILModule.cs
+++ neon/MSIL/ILModule.cs
@@ -12,9 +12,10 @@ namespace Neo.Compiler.MSIL
         public Mono.Cecil.ModuleDefinition module = null;
         public List<string> moduleref = new List<string>();
         public Dictionary<string, ILType> mapType = new Dictionary<string, ILType>();
-        public ILModule()
+        public ILogger logger;
+        public ILModule(ILogger _logger)
         {
-
+          this.logger = _logger;
         }
         public void LoadModule(System.IO.Stream dllStream, System.IO.Stream pdbStream)
         {
